d6840156ec02d011d6e36e345cf7171f0e9edda4
usmonov <usmonovd94@gmail.com>
2025-02-18 11:33:35 +0500

    migation

---
+++ SkyLinkApi/Data/AppDbContext.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using SkyLinkApi.Entity;
+
+namespace SkyLinkApi.Data;
+
+public partial class AppDbContext : DbContext
+{
+    public IServiceScopeFactory _serviceScopeFactory;
+
+    [ActivatorUtilitiesConstructor]
+    public AppDbContext(DbContextOptions<AppDbContext> options,
+        IServiceScopeFactory serviceScopeFactory) : base(options)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    public DbSet<FlightEntity> Flights { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<FlightEntity>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("flight_pkey");
+
+            entity.ToTable("flights");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("id");
+            entity.Property(e => e.Airline).HasColumnName("airline");
+            entity.Property(e => e.Price).HasColumnName("price");
+            entity.Property(e => e.DepartureAirportCode).HasColumnName("departure_airport_code");
+            entity.Property(e => e.DestinationAirportCode).HasColumnName("destination_airport_code");
+            entity.Property(e => e.DepartureDate).HasColumnName("departure_date");
+            entity.Property(e => e.ArrivalDate).HasColumnName("arrival_date");
+            entity.Property(e => e.Layovers).HasColumnName("layovers").HasDefaultValue(0);
+
+            entity.HasIndex(e => e.DepartureAirportCode);
+            entity.HasIndex(e => e.DestinationAirportCode);
+            entity.HasIndex(e => e.Airline);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+}
---
+++ SkyLinkApi/Data/Migrations/20250218061037_AddFlightsTable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SkyLinkApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFlightsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "flights",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    airline = table.Column<string>(type: "text", nullable: false),
+                    price = table.Column<double>(type: "double precision", nullable: false),
+                    departure_airport_code = table.Column<string>(type: "text", nullable: false),
+                    destination_airport_code = table.Column<string>(type: "text", nullable: false),
+                    departure_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    arrival_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    layovers = table.Column<int>(type: "integer", nullable: false, defaultValue: 0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("flight_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_flights_airline",
+                table: "flights",
+                column: "airline");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_flights_departure_airport_code",
+                table: "flights",
+                column: "departure_airport_code");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_flights_destination_airport_code",
+                table: "flights",
+                column: "destination_airport_code");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "flights");
+        }
+    }
+}
