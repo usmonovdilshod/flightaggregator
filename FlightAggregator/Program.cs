25a93cc4f99383df8e566792c8305442c8c47f6c
usmonov <usmonovd94@gmail.com>
2025-02-19 08:39:07 +0500

    google auth

--- FlightAggregator/Program.cs
+++ FlightAggregator/Program.cs
@@ -2,6 +2,8 @@
 using FlightAggregator.Components;
 using FlightAggregator.Data;
 using FlightAggregator.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,18 +16,32 @@ builder.Services.AddDbContextFactory<AppDbContext>(options =>
 #endregion
 
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie() 
+.AddGoogle(options =>
+{
+    options.ClientId = builder.Configuration["Authentication:Google:ClientId"]!;
+    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"]!;
+});
 
 
 
 builder.Services.AddMemoryCache();
 builder.Services.AddSingleton<ISearchService, SearchService>();
 builder.Services.AddLogging();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
@@ -40,6 +56,9 @@ app.UseAntiforgery();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 var dbContextFactory = app.Services.GetRequiredService<IDbContextFactory<AppDbContext>>();
 using var dbContext = dbContextFactory.CreateDbContext();
